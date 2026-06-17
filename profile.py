43ad6b7f8be77382359d234019215e4303876f37
Lamonteclark00core <lamonteclark00@gmail.com>
2026-06-17 10:55:25 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,6 @@
+full_name = "Lamonte Clark"
+nickname = "Monte"
+age = 24
+used_python_before = False
+hobbies = ["Basketball", "Video Games", "Gym"]
+favorites = {"movie": "Black Panther", "Food": "Barbeque", "Hobby": "Video Games"}
